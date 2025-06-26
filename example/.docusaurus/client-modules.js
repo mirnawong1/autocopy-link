6f6e490a5447d6d803e1c47e0a4736b3888b08f1
mirnawong1 <mirnawong@gmail.com>
2025-06-26 11:57:32 +0100

    Add example directory and gitignore

---
+++ example/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/mirnawong/autocopy-link/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/mirnawong/autocopy-link/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/mirnawong/autocopy-link/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/mirnawong/autocopy-link/example/src/css/custom.css"),
+];
